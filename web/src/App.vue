4ee2af4e27519c2ccc9f0f1bd0dd461680ce342c
Liao Yin <1691156671@qq.com>
2023-03-03 21:02:10 +0800

    version5.0 前端地图和导航栏

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <div>
-    <div>Bot昵称:{{bot_name}}</div>
-    <div>Bot战力:{{bot_rating}}</div>
-  </div>
+  <NavBar />
 
 
   <router-view/>
@@ -11,30 +8,14 @@
 
 <script>
 
+import NavBar from './components/NavBar.vue';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-import $ from 'jquery';
-import {ref} from 'vue';
 
 export default{
-  name: "App",
-  setup:()=>{
-    let bot_name=ref("");
-    let bot_rating=ref("");
-
-    $.ajax({
-      url: "http://127.0.0.1:3000/pk/getbotinfo/",
-      type: "get",
-      success: resp =>{
-        //console.log(resp);
-        bot_name.value=resp.name;
-        bot_rating.value=resp.rating;
-      }
-    });
-
-    return {
-      bot_name,
-      bot_rating,
-    }
+  components:{
+    NavBar,
   }
 }
 
@@ -45,8 +26,7 @@ export default{
 <style>
 
 body{
-  background-image: url("@/assets/background.png");
-  background-repeat: no-repeat;
+  background-image: url("@/assets/images/background.png");
   background-size: cover;
 }
 
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,6 +1,46 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import PkIndexView from '../views/pk/PkindexView.vue';
+import RecordIndexView from '../views/record/RecordindexView.vue';
+import RankListIndexView from '../views/ranklist/RanklistindexView.vue';
+import UserBotIndexView from '../views/user/bot/UserBotindexView.vue';
+import NotFound from '../views/error/NotFound.vue';
+
 
 const routes = [
+  {
+    path: "/",
+    name: "home",
+    redirect: "/pk/",
+  },
+  {
+    path: "/pk/",
+    name: "pk_index",
+    component: PkIndexView,
+  },
+  {
+    path: "/record/",
+    name: "record_index",
+    component: RecordIndexView,
+  },
+  {
+    path: "/ranklist/",
+    name: "ranklist_index",
+    component: RankListIndexView,
+  },
+  {
+    path: "/user/bot/",
+    name: "user_bot_index",
+    component: UserBotIndexView,
+  },
+  {
+    path: "/404/",
+    name: "404",
+    component: NotFound,
+  },
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/404/",
+  }
 ]
 
 const router = createRouter({
