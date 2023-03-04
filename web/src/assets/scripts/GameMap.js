fe88024559e5b4c687e64d551a5c11583d466869
Liao Yin <1691156671@qq.com>
2023-03-04 12:07:32 +0800

    version7.0 前端改良地图地图加蛇润色蛇检测非法蛇加眼睛

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -1,5 +1,6 @@
 import { AcGameObject } from "./AcGameObject";
 import {Wall} from "./Wall";
+import { Snake } from "./Snake";
 
 
 export class GameMap extends AcGameObject{
@@ -10,11 +11,17 @@ export class GameMap extends AcGameObject{
         this.L=0;//一个单位长度
 
         this.rows=13;
-        this.cols=13;
+        this.cols=14;
 
         this.inner_walls_count=20;
 
-        this.wall=[];
+        this.walls=[];
+
+        this.snakes=[
+            new Snake({id: 0,color: "#4876EC",r:this.rows-2,c:1},this),
+            new Snake({id: 1,color: "#F94848",r:1,c:this.cols-2},this),
+        ]
+
 
     }
 
@@ -59,11 +66,11 @@ export class GameMap extends AcGameObject{
             for(let j=0;j<1000;j++){
                 let r=parseInt(Math.random()*this.rows);
                 let c=parseInt(Math.random()*this.cols);
-                if(g[r][c]||g[c][r])
+                if(g[r][c]||g[this.rows-1-r][this.cols-1-c])
                     continue;
                 if(r===this.rows-2&&c==1||r==1&&c==this.cols-2)
                     continue;
-                g[c][r]=g[r][c]=true;
+                g[c][r]=g[this.rows-1-r][this.cols-1-c]=true;
                 break;
             }
         }
@@ -74,11 +81,34 @@ export class GameMap extends AcGameObject{
         for(let r=0;r<this.rows;r++){
             for(let c=0;c<this.cols;c++){
                 if(g[r][c]){
-                    this.wall.push(new Wall(r,c,this));
+                    this.walls.push(new Wall(r,c,this));
                 }
             }
         }
         return true;
+    }
+
+    add_listening_events(){
+        this.ctx.canvas.focus();
+        const [snake0,snake1]=this.snakes;
+        this.ctx.canvas.addEventListener('keydown',e=>{
+            if(e.key==='w' || e.key==='W')
+                snake0.set_direction(0);
+            else if(e.key==='d'|| e.key==='D')
+                snake0.set_direction(1);
+            else if(e.key==='s' ||e.key==='S')
+                snake0.set_direction(2);
+            else if(e.key==='a'|| e.key==='A')
+                snake0.set_direction(3);
+            else if(e.key==='ArrowUp')
+                snake1.set_direction(0);
+            else if(e.key==='ArrowRight')
+                snake1.set_direction(1);
+            else if(e.key==='ArrowDown')
+                snake1.set_direction(2);
+            else if(e.key==='ArrowLeft')
+                snake1.set_direction(3);
+        });
     }
 
     start(){
@@ -86,6 +116,7 @@ export class GameMap extends AcGameObject{
             if(this.create_walls())
                 break;
         }
+        this.add_listening_events();
     }
 
     update_size(){
@@ -94,8 +125,51 @@ export class GameMap extends AcGameObject{
         this.ctx.canvas.height=this.L*this.rows;
     }
 
+    
+
+    check_ready(){//判断两条蛇是否都准备下一回合
+        for(const snake of this.snakes){
+            if(snake.status!=="idle")
+                return false;
+            if(snake.direction===-1)
+                return false;
+        }
+        return true;
+    }
+
+    check_valid(cell){//检测目标位置是否合法,没有撞到两条蛇的身体和障碍物
+        for(const wall of this.walls){
+            if(wall.r===cell.r && wall.c===cell.c)
+                return false;
+        }
+        for(const snake of this.snakes){
+            let k=snake.cells.length;
+            if(!snake.check_tail_increasing()){//当蛇尾会前进的话,不用判断
+                k--;
+            }
+
+            for(let i=0;i<k;i++){
+                if(snake.cells[i].r===cell.r && snake.cells[i].c===cell.c)
+                    return false;
+            }
+
+        }
+        return true;
+
+    }
+    
+    next_step(){//让两条蛇进入下一回合
+        for(const snake of this.snakes){
+            snake.next_step();
+        }
+
+    }
+
     update(){
         this.update_size();
+        if(this.check_ready()){
+            this.next_step();
+        }
         this.render();
     }
 
@@ -116,9 +190,5 @@ export class GameMap extends AcGameObject{
         }
 
     }
-
-
-
-
 
 }
